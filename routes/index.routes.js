398fd8ba9f3c77dfd48561ec51c0d3145aeefdf7
hungndweq <thang7781@gmail.com>
2025-01-07 22:49:04 +0700

    Load files and updated realtime

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -2,19 +2,29 @@ const express = require('express');
 const router = express.Router();
 const multer = require('multer');
 const { createClient } = require('@supabase/supabase-js');
-require('dotenv').config(); // Load biến môi trường từ file .env
+require('dotenv').config();
+const fileModel = require('../models/files.model');
+const authMiddleware = require('../middleware/auth');
 
-// URL và Key của dự án Supabase từ biến môi trường
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 const upload = multer();
 
-router.get('/home', (req, res) => {
-    res.render('home');
+router.get('/home', authMiddleware, async (req, res) => {
+    try {
+        const userFiles = await fileModel.find({ user: req.user.userid });
+        console.log(userFiles);
+        res.render('home', {
+            files: userFiles
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('An error occurred while fetching files.');
+    }
 });
 
-router.post('/upload-file', upload.single('file'), async (req, res) => {
+router.post('/upload-file', authMiddleware, upload.single('file'), async (req, res) => {
     const file = req.file;
 
     if (!file) {
@@ -23,7 +33,7 @@ router.post('/upload-file', upload.single('file'), async (req, res) => {
 
     try {
         const { data, error } = await supabase.storage
-            .from('test')
+            .from('test') 
             .upload(`uploads/${file.originalname}`, file.buffer, {
                 cacheControl: '3600',
                 upsert: false
@@ -32,6 +42,17 @@ router.post('/upload-file', upload.single('file'), async (req, res) => {
         if (error) {
             throw error;
         }
+
+        // Lưu thông tin file vào MongoDB
+        const newFile = new fileModel({
+            path: data.path,
+            originalname: file.originalname,
+            user: req.user.userid
+        });
+
+        await newFile.save();
+
+        console.log('File saved to MongoDB:', newFile);
 
         res.status(200).send('File uploaded successfully.');
     } catch (error) {
