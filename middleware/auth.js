c9c53552397415b98820d9731e4599d477b910cf
hungndweq <thang7781@gmail.com>
2025-01-08 23:11:19 +0700

    Updated

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,19 +1,23 @@
 const jwt = require("jsonwebtoken");
 
 const authMiddleware = (req, res, next) => {
-  const token = req.cookies.token || req.headers["authorization"];
+    // Get token from cookies or headers
+    const token = req.cookies.token || req.headers["authorization"];
 
-  if (!token) {
-    return res.status(401).send("Access denied. No token provided.");
-  }
+    // If no token is found, return response without access
+    if (!token) {
+        return res.status(401).send("Access denied. No token provided.");
+    }
 
-  try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = decoded; // Gán thông tin người dùng vào req.user
-    next();
-  } catch (error) {
-    res.status(400).send("Invalid token.");
-  }
+    try {
+        // Verify token
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = decoded; // Assign user data to req.user
+        // Call next middleware
+        next();
+    } catch (error) {
+        res.status(400).send("Invalid token.");
+    }
 };
 
 module.exports = authMiddleware;
